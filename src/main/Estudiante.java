28831213d9fa3a775eeef1a7a838daac88175ff1
Esteban Secaida <esecaidavargas@gmail.com>
2022-08-30 23:11:46 -0600

    Listo, se proyectó todo, toca terminar de escribir lineas para los demas atributos, viva diosito

--- src/main/Estudiante.java
+++ src/main/Estudiante.java
@@ -1,7 +1,8 @@
 package main;
 
+import javafx.application.Application;
+
 import java.io.File;
-import java.lang.reflect.Constructor;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -20,6 +21,8 @@ public abstract class Estudiante {
     private Integer notaP1;
     private Integer notaP2;
     private Integer notaP3;
+    ArrayList<EstudianteA> estudiA = new ArrayList<>();
+    ArrayList<EstudianteB> estudiB = new ArrayList<>();
 
     public String getCarne() {
         return carne;
@@ -102,62 +105,5 @@ public abstract class Estudiante {
     }
     public abstract double notaPromedio();
 
-    public double notaFinal(){
-        double res0 = 0.0;
-        res0 = getNotaPromedioE() + getNotaPromedioQ() + getNotaPromedioT() + 1000.0;
-        return res0;
-    }
-
-    public static void main(String[] args) throws Exception {
-        int var = 1;
-        int con = 0;
-
-        ArrayList<EstudianteA> estudiA = new ArrayList<>();
-        ArrayList<EstudianteB> estudiB = new ArrayList<>();
-
-        List<String> listaBacana = new ArrayList<>();
-        Scanner lector = new Scanner(new File("C:\\Users\\eseca\\IdeaProjects\\Tarea1Datos1\\TablaEstudiantes.csv"));
-        lector.useDelimiter("[,:\r\n]+");
-        while (lector.hasNext()) {
-            if (var <= 1) {
-                lector.nextLine();
-                var++;
-            }
-            if (con == 12) {
-                if (Objects.equals(listaBacana.get(5), "A")){
-                    estudiA.add(new EstudianteA(listaBacana.get(0), listaBacana.get(1), listaBacana.get(2), listaBacana.get(3),
-                            listaBacana.get(4), listaBacana.get(5), Integer.parseInt(listaBacana.get(6)), Integer.parseInt(listaBacana.get(7)),
-                            Integer.parseInt(listaBacana.get(8)), Integer.parseInt(listaBacana.get(9)), Integer.parseInt(listaBacana.get(10)),
-                            Integer.parseInt(listaBacana.get(11))));
-                }
-                else{
-                    estudiB.add(new EstudianteB(listaBacana.get(0), listaBacana.get(1), listaBacana.get(2), listaBacana.get(3),
-                            listaBacana.get(4), listaBacana.get(5), Integer.parseInt(listaBacana.get(6)), Integer.parseInt(listaBacana.get(7)),
-                            Integer.parseInt(listaBacana.get(8)), Integer.parseInt(listaBacana.get(9)), Integer.parseInt(listaBacana.get(10)),
-                            Integer.parseInt(listaBacana.get(11))));
-                }
-                listaBacana.clear();
-                con = 0;
-                lector.nextLine();
-            } else {
-                listaBacana.add(lector.next());
-                con++;
-            }
-        }
-        if (Objects.equals(listaBacana.get(5), "A")){
-            estudiA.add(new EstudianteA(listaBacana.get(0), listaBacana.get(1), listaBacana.get(2), listaBacana.get(3),
-                    listaBacana.get(4), listaBacana.get(5), Integer.parseInt(listaBacana.get(6)), Integer.parseInt(listaBacana.get(7)),
-                    Integer.parseInt(listaBacana.get(8)), Integer.parseInt(listaBacana.get(9)), Integer.parseInt(listaBacana.get(10)),
-                    Integer.parseInt(listaBacana.get(11))));
-        }
-        else{
-            estudiB.add(new EstudianteB(listaBacana.get(0), listaBacana.get(1), listaBacana.get(2), listaBacana.get(3),
-                    listaBacana.get(4), listaBacana.get(5), Integer.parseInt(listaBacana.get(6)), Integer.parseInt(listaBacana.get(7)),
-                    Integer.parseInt(listaBacana.get(8)), Integer.parseInt(listaBacana.get(9)), Integer.parseInt(listaBacana.get(10)),
-                    Integer.parseInt(listaBacana.get(11))));
-        }
-        System.out.print(estudiA.get(1).notaPromedio() + "\n");
-        System.out.print(estudiB.get(0).notaFinal());
-        lector.close();
-    }
+    public abstract double notaFinal();
 }
--- src/main/MainController.java
+++ src/main/MainController.java
@@ -1,14 +1,121 @@
 package main;
 
+import javafx.beans.property.ReadOnlyIntegerProperty;
+import javafx.beans.property.ReadOnlyStringProperty;
+import javafx.beans.property.ReadOnlyStringWrapper;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
-public class MainController {
-    @FXML
-    private Label welcomeText;
+import java.awt.event.ActionEvent;
+import java.io.File;
+import java.net.URL;
+import java.util.*;
 
+public class MainController implements Initializable {
+    private ObservableList<EstudianteA> estudianteAS;
+    private ObservableList<EstudianteB> estudianteBS;
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Test!");
+    private TableView<EstudianteA> diosVen;
+    @FXML
+    private TableColumn<EstudianteA, String> slotCarne;
+    @FXML
+    private TableColumn<EstudianteA, String> slotNombre;
+    @FXML
+    private TableColumn<EstudianteA, String> slotCorreo;
+    @FXML
+    private TableColumn<EstudianteA, String> slotTelef;
+    @FXML
+    private TableColumn<EstudianteA, String> slotNickname;
+    @FXML
+    private TableColumn<EstudianteA, String> slotTipo;
+    @FXML
+    private TableColumn<EstudianteA, Integer> slotNPE;
+    @FXML
+    private TableColumn<EstudianteA, Integer> slotNPQ;
+    @FXML
+    private TableColumn<EstudianteA, Integer> slotNPT;
+    @FXML
+    private TableColumn<EstudianteA, Integer> slotNP1;
+    @FXML
+    private TableColumn<EstudianteA, Integer> slotNP2;
+    @FXML
+    private TableColumn<EstudianteA, Integer> slotNP3;
+    @FXML
+    private TableColumn<EstudianteA, Integer> slotPA;
+    @FXML
+    private TableColumn<EstudianteA, Integer> slotPB;
+    @FXML
+    private TableColumn<EstudianteA, Integer> slotFinal;
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        estudianteBS = FXCollections.observableArrayList();
+        estudianteAS = FXCollections.observableArrayList();
+        this.slotCarne.setCellValueFactory(new PropertyValueFactory<>("carne"));
+        this.slotNombre.setCellValueFactory(new PropertyValueFactory<>("nombreApellidos"));
+        this.slotCorreo.setCellValueFactory(new PropertyValueFactory<>("correo"));
+        this.slotTelef.setCellValueFactory(new PropertyValueFactory<>("telefono"));
+        this.slotNickname.setCellValueFactory(new PropertyValueFactory<>("nickname"));
+        this.slotTipo.setCellValueFactory(new PropertyValueFactory<>("tipoEstudiante"));
+        this.slotNPE.setCellValueFactory(new PropertyValueFactory<>("correo"));
+
+    }
+    @FXML
+    private void empezarFiesta() throws Exception{
+        int var = 1;
+        int con = 0;
+
+        List<String> listaBacana = new ArrayList<>();
+        Scanner lector = new Scanner(new File("C:\\Users\\eseca\\IdeaProjects\\Tarea1Datos1\\TablaEstudiantes.csv"));
+        lector.useDelimiter("[,:\r\n]+");
+        while (lector.hasNext()) {
+            if (var <= 1) {
+                lector.nextLine();
+                var++;
+            }
+            if (con == 12) {
+                if (Objects.equals(listaBacana.get(5), "A")){
+                    estudianteAS.add(new EstudianteA(listaBacana.get(0), listaBacana.get(1), listaBacana.get(2), listaBacana.get(3),
+                            listaBacana.get(4), listaBacana.get(5), Integer.parseInt(listaBacana.get(6)), Integer.parseInt(listaBacana.get(7)),
+                            Integer.parseInt(listaBacana.get(8)), Integer.parseInt(listaBacana.get(9)), Integer.parseInt(listaBacana.get(10)),
+                            Integer.parseInt(listaBacana.get(11))));
+                }
+                else{
+                    estudianteBS.add(new EstudianteB(listaBacana.get(0), listaBacana.get(1), listaBacana.get(2), listaBacana.get(3),
+                            listaBacana.get(4), listaBacana.get(5), Integer.parseInt(listaBacana.get(6)), Integer.parseInt(listaBacana.get(7)),
+                            Integer.parseInt(listaBacana.get(8)), Integer.parseInt(listaBacana.get(9)), Integer.parseInt(listaBacana.get(10)),
+                            Integer.parseInt(listaBacana.get(11))));
+                }
+                listaBacana.clear();
+                con = 0;
+                lector.nextLine();
+            } else {
+                listaBacana.add(lector.next());
+                con++;
+            }
+        }
+        if (Objects.equals(listaBacana.get(5), "A")){
+            estudianteAS.add(new EstudianteA(listaBacana.get(0), listaBacana.get(1), listaBacana.get(2), listaBacana.get(3),
+                    listaBacana.get(4), listaBacana.get(5), Integer.parseInt(listaBacana.get(6)), Integer.parseInt(listaBacana.get(7)),
+                    Integer.parseInt(listaBacana.get(8)), Integer.parseInt(listaBacana.get(9)), Integer.parseInt(listaBacana.get(10)),
+                    Integer.parseInt(listaBacana.get(11))));
+        }
+        else{
+            estudianteBS.add(new EstudianteB(listaBacana.get(0), listaBacana.get(1), listaBacana.get(2), listaBacana.get(3),
+                    listaBacana.get(4), listaBacana.get(5), Integer.parseInt(listaBacana.get(6)), Integer.parseInt(listaBacana.get(7)),
+                    Integer.parseInt(listaBacana.get(8)), Integer.parseInt(listaBacana.get(9)), Integer.parseInt(listaBacana.get(10)),
+                    Integer.parseInt(listaBacana.get(11))));
+        }
+        System.out.print(estudianteAS.get(1).notaPromedio() + "\n");
+        System.out.print(estudianteBS.get(0).notaFinal());
+        this.diosVen.setItems(estudianteAS);
+        lector.close();
     }
 }
